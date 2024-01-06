8c921f45c24f5be0a4e36906d68b8919cb2d03d4
HYSTERIA <129130014+hysteria222@users.noreply.github.com>
2024-01-05 21:20:46 -0600

    pushing final

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Card from './Card'
+
+
+const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
+
 
 function App() {
+
+  const [spaceInfo, setSpaceInfo] = useState()
+
+  useEffect(() => {
+    axios.get(url)
+      .then(res => {
+        console.log(res.data)
+        setSpaceInfo(res.data)
+      }).catch(err => console.log(err.message))
+  }, [])
+
+if (!spaceInfo) return 'Fetching photo of the day'
+
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
+    <section>
+     <Card 
+     title={spaceInfo.title}
+     text={spaceInfo.explanation}
+     imageURL={spaceInfo.url}
+     date={spaceInfo.date}
+
+
+     />
+    </section>
   )
 }
 
